1160dfd639b15240ae12a639ff5b94d1721cf4bb
Silond <silond@gmail.com>
2020-06-26 12:09:05 -0500

    chuck is crying

--- src/test/java/StepDefenitions/chuckNorris.java
+++ src/test/java/StepDefenitions/chuckNorris.java
@@ -4,5 +4,8 @@ public class chuckNorris {
 
     public static void main(String[] args) {
         System.out.println("Let's practice guys!!!!!");
+        System.out.println("Please leave Chuck alone, otherwise he's going to cry!!!!!");
+
+        System.out.println("Please leave Chuck alone, otherwise he's going to cry!!!!!");
     }
 }
